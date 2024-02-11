545d6162463d8aa6a0d05923ef0ee28ecf358247
Diego Pavani <diego.pavani@jackinthebox.com>
2024-02-11 16:29:11 -0300

    added gtm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { BuscaCepForm } from "@/components/buscaCepForm/buscaCepForm";
 import { Header } from "@/components/header/header";
 import mixpanel from "mixpanel-browser";
+import Script from "next/script";
 
 export default function Home() {
   mixpanel.init("3123dfec454ea69f5e65f6d8498b918a", {
@@ -13,6 +14,14 @@ export default function Home() {
   mixpanel.track("Page view", {});
   return (
     <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KNW3V6JX');`}
+      </Script>
       <Header />
       <div className="flex flex-col items-center mt-2">
         <div
