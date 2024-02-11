1990a7ac3a39471ad2f3838cc8361b32ff8a7754
Diego Pavani <diego.pavani@jackinthebox.com>
2024-02-11 16:27:27 -0300

    added google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "../app/globals.css";
 import mixpanel from "mixpanel-browser";
 
@@ -10,7 +11,19 @@ function MyApp({ Component, pageProps }) {
     ignore_dnt: true,
   });
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KNW3V6JX');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
